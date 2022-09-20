c8177dc8002581c807843928cdf24d298a9cf7ac
Sergiy <4green4black@gmail.com>
2022-09-20 21:59:37 +0300

    add menu

--- nam/main.js
+++ nam/main.js
@@ -1,13 +1,18 @@
-alert ('Whats up!')
+const menu = document.querySelector('.header-list')
+const menuOpen = document.querySelector('.open_menu')
+const menuClose = document.querySelector('.close_menu')
 
-const myCity = {
-   city: 'NewYork'
+menuOpen.onclick = function(){
+   menu.classList.add('open')
+   menuOpen.classList.add('open')
+   menuClose.classList.add('open')
+   document.body.style.overflow = 'hidden'
+}
+menuClose.onclick = function(){
+   menu.classList.remove('open')
+   menuOpen.classList.remove('open')
+   menuClose.classList.remove('open')
+   document.body.style.overflow = ''
+   
 }
 
-myCity.popular = true
-
-console.log(myCity)
-
-myCity.country = 'USA'
-
-console.log(myCity)
