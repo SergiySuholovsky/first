da8a08efce783e8cc9a53935d471218c8ff828dc
serg <4green4black@gmail.com>
2023-03-08 20:11:47 +0200

    add mobile, tablet, mobile menu

---
+++ onlinezoo/main.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('.header-list')
+const menuOpen = document.querySelector('.open_menu')
+const menuClose = document.querySelector('.close_menu')
+
+menuOpen.onclick = function(){
+   menu.classList.add('open')
+   menuOpen.classList.add('open')
+   menuClose.classList.add('open')
+   document.body.style.overflow = 'hidden'
+}
+menuClose.onclick = function(){
+   menu.classList.remove('open')
+   menuOpen.classList.remove('open')
+   menuClose.classList.remove('open')
+   document.body.style.overflow = ''
+   
+}
